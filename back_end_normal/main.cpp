9d784bb83ac5cdb51dad9c0269a45a43dc562c99
lillian039 <1912383615@qq.com>
2022-06-16 11:04:47 +0800

    normal

--- back_end_normal/main.cpp
+++ back_end_normal/main.cpp
@@ -6,8 +6,6 @@ using std::string;
 
 int main() {
     std::ios::sync_with_stdio(false);
-//    freopen("5.in", "r", stdin);
-//    freopen("me.out", "w", stdout);
     string commandLine;
     while (true) {
         getline(std::cin, commandLine);
